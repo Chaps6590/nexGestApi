3defb6c46ecb0ce3385db7f79c606fb033518a20
csarchioni <csarchioni@ater.gob.ar>
2025-05-15 08:14:57 -0300

    Creacion de carpeta dist

---
+++ dist/routes/auth.routes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const auth_controller_1 = require("../controllers/auth.controller");
+const requireAuth_1 = require("../middleware/requireAuth");
+const router = (0, express_1.Router)();
+router.post("/login", auth_controller_1.loginHandler);
+router.get("/profile", requireAuth_1.requireAuth, auth_controller_1.profileHandler);
+router.post("/register", auth_controller_1.registerHandler);
+exports.default = router;
